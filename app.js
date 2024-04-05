fa4fa210155a0695bd8c4a0109555a34fb1327d4
Usman Khan <160907897+usmankhan0016@users.noreply.github.com>
2024-04-05 04:53:13 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let guest_list = ["Ahsan", "Umar", "Talib"];
+for (let i = 0; i < guest_list.length; i++) {
+    console.log(`Respected Sir ${guest_list[i]}` + "\nYou are invited for the dinner tomorrow.\n");
+}
---
+++ app.ts
@@ -0,0 +1,4 @@
+let guest_list: string[]= ["Ahsan","Umar","Talib"];
+for(let i=0; i<guest_list.length;i++){
+    console.log(`Respected Sir ${guest_list[i]}` + "\nYou are invited for the dinner tomorrow.\n");
+}
